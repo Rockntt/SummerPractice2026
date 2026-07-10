9669486c898c752f60239861e984f0676ea4dabb
Makar Klochkov <114337126+Rockntt@users.noreply.github.com>
2026-07-11 00:03:25 +0300

    Create 3A.cpp

---
+++ 3A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    for (int i = 0; i < n; ++i) {
+        if (i) cout << ' ';
+        cout << 1;
+    }
+    cout << '\n';
+ 
+    return 0;
+}
