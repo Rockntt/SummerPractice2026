0298365fa026cc26659ed89aa3c1e7ded7aca736
Makar Klochkov <114337126+Rockntt@users.noreply.github.com>
2026-07-11 00:14:21 +0300

    Update 5B.cpp

--- 5B.cpp
+++ 5B.cpp
@@ -1,1 +1,39 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        if (n == 0) {
+            cout << 10 << '\n';
+            continue;
+        }
+ 
+        if (n == 1) {
+            cout << 1 << '\n';
+            continue;
+        }
+ 
+        vector<int> digits;
+        for (int d = 9; d >= 2; --d) {
+            while (n % d == 0) {
+                digits.push_back(d);
+                n /= d;
+            }
+        }
+ 
+        if (n > 1) {
+            cout << -1 << '\n';
+        } else {
+            sort(digits.begin(), digits.end());
+            for (int d : digits) cout << d;
+            cout << '\n';
+        }
+    }
+ 
+    return 0;
+}
